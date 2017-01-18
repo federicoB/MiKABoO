184abaa43d0333b417e8bf91f54fb94fd70832a4
federicoB <federicobertani96@gmail.com>
2017-01-18 13:47:59 +0100

    Added scheduler queue handlers

--- tcbImpl.c
+++ tcbImpl.c
@@ -1,1 +1,23 @@
+#include "mikabooq.h"
 
+void thread_enqueue(struct tcb_t *new, struct list_head *queue) {
+    list_add_tail(&(new->t_sched),queue);
+}
+
+struct tcb_t *thread_qhead(struct list_head *queue) {
+    struct tcb_t * result = NULL;
+    if (!list_empty(queue)) {
+        result = container_of(list_next(queue),tcb_t,t_sched);
+    }
+    return result;
+}
+
+struct tcb_t *thread_dequeue(struct list_head *queue) {
+    struct tcb_t* result = NULL;
+    if (!list_empty(queue)) {
+        struct list_head * first = list_next(queue);
+        result = container_of(first,tcb_t,t_sched);
+        list_del(first);
+    }
+    return result;
+}
