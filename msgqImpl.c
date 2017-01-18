c30441607022635ac39088f244a84fd478a7b2e4
Riccardo Maffei <RiccardoMaffei@users.noreply.github.com>
2017-01-18 20:41:50 +0100

    Improved code readability in msgqImpl.c. Improved documentation in msgqImpl.c. Fixed some minor bugs in msgqImpl.c. Improved code structure in msgqImpl.c.

--- msgqImpl.c
+++ msgqImpl.c
@@ -2,81 +2,105 @@
 #include "const.h"
 #include "listx.h"
 
-struct msg_t msgArray[MAXMSG];
-struct list_head freeMSG;
-
+/* extracts a message from the free message list. Must check size first. */
 struct msg_t* extractMSG();
 
+//the array of messages
+struct msg_t msgArray[MAXMSG];
+//message free list entry point
+struct list_head freeMSG;
 
 void msgq_init(void){
-	if(MAXMSG <= 0) exit();
-	INIT_LIST_HEAD(&freeMSG);
-	for(int i = 0; i < MAXMSG;i++){
-		list_add(&(msgArray[i].m_next),&freeMSG);
-	}
+    //if invalid const exit
+    if(MAXMSG <= 0) exit();
+    //init the free list
+    INIT_LIST_HEAD(&freeMSG);
+    //for the whole array of messages
+    for(int i = 0; i < MAXMSG;i++){
+        //add the message to the free list
+        list_add(&(msgArray[i].m_next), &freeMSG);
+    }
+}
+
+int msgq_add(struct tcb_t* sender, struct tcb_t* destination, uintptr_t value){
+    //the result (default: error)
+    int result = -1;
+    //if sender and destination are not null and there is a free message available
+    if((sender != NULL && destination != NULL && !list_empty(&freeMSG))){
+        //extract a message
+        struct msg_t* newMsg = extractMSG();
+        //set the sender
+        newMsg->m_sender = sender;
+        //set the value
+        newMsg->m_value = value;
+        //enqueue the message (tail add to the list)
+        list_add_tail(&(newMsg->m_next), &(destination->t_msgq));
+        //set the result as success
+        result = 0;
+    }
+    //return the result
+    return result;
 }
 
 
-int msgq_add(struct tcb_t *sender, struct tcb_t *destination, uintptr_t value){
-	int result = -1;
-	if((sender != NULL && destination != NULL && !list_empty(&freeMSG))){
-		struct msg_t* newMsg = extractMSG();
-		newMsg->m_sender = sender;
-		newMsg->m_value = value;
-		list_add_tail(&(newMsg->m_next),&(destination->t_msgq)); //append the message to destination's message queue
-		result = 0;
-	}
-	return result;
-
-}
-
-/*  
-	Retrieves a message for the given destination, stores result in *value
-	if sender == NULL: gets a message from any sender
-	if sender != NULL && *sender == NULL: gets a message from any sender and stores sender tcb's address in *sender
-	if sender != NULL && *sender != NULL: gets a message sent by a specific *sender
-	Returns:
-		0 if everything successful
-		-1 if there was an error
-*/
-int msgq_get(struct tcb_t **sender, struct tcb_t *destination, uintptr_t *value){
-	if(destination != NULL){
-		struct msg_t* extractedMSG = NULL;
-		if(sender == NULL){
-			extractedMSG = container_of(list_next(&(destination->t_msgq)),msg_t,m_next);
-		}
-		else if(sender != NULL && *sender == NULL){
-			extractedMSG = container_of(list_next(&(destination->t_msgq)),msg_t,m_next);
-			*sender = extractedMSG->m_sender;
-		}
-		else if(sender != NULL && *sender != NULL){
-        	struct msg_t* msgIter;
-        	list_for_each_entry(msgIter,list_next(&(destination->t_msgq)),m_next) {
-                if(curMsg->m_sender == *sender){
-                	//if a message from given sender is found
-                	extractedMSG = curMsg;
-                	break;
+int msgq_get(struct tcb_t** sender, struct tcb_t* destination, uintptr_t* value){
+    //the result (default: error)
+    int result = -1;
+    //if the given destination is not null and its message queue is not empty
+    if(destination != NULL &&  !list_empty(&(destination->t_msgq))){
+        //init the selected message as null
+        struct msg_t* selectedMSG = NULL;
+        //if sender is null (select any message)
+        if(sender == NULL){
+            //get the first message
+            selectedMSG = container_of(list_next(&(destination->t_msgq)), msg_t, m_next);
+        }
+        //else if sender is not null but *sender is null (pointer to a null nointer)
+        //(select any message and save the message sender)
+        else if(sender != NULL && *sender == NULL){
+            //get the first message
+            selectedMSG = container_of(list_next(&(destination->t_msgq)), msg_t, m_next);
+            //save the message sender
+            *sender = selectedMSG->m_sender;
+        }
+        //else if a specific sender is given (pointer to a non-null pointer)
+        //(select a message coming from the given sender)
+        else if(sender != NULL && *sender != NULL){
+            //declare the message iterator
+            struct msg_t* curMessage;
+            //while there is an other message and the current message sender is not
+            //the given one. (implemented using the "for each entry" macro and a break)
+            list_for_each_entry(curMessage,list_next(&(destination->t_msgq)), m_next) {
+                //if the sender of the current message equals the given sender
+                if(curMessage->m_sender == *sender){
+                    //set the selected message as the current one
+                    selectedMSG = curMessage;
+                    //stop the loop using a break
+                    break;
                 }
-        	}
-		}
-
-		if(extractedMSG != NULL){ 
-			*value = extractedMSG->m_value;
-			list_del(&(extractedMSG->m_next));
-		}
-		else{
-			//error, message not found
-			return -1;
-		}
-	}
-	else{
-		//error destination==NULL
-		return -1;
-	}
+            }
+        }
+        //if a message has been selected
+        if(selectedMSG != NULL){
+            //get and save the message value
+            *value = selectedMSG->m_value;
+            //delete the message from the queue
+            list_del(&(selectedMSG->m_next));
+            //add the message to the message free list
+            list_add(&(selectedMSG->m_next), &freeMSG);
+            //set result as zero (success)
+            result = 0;
+        }//else leave the result as error
+    }//else leave the result as error
+    //return result
+    return result;
 }
 
 struct msg_t* extractMSG(){
-	struct msg_t* extractedMSG = container_of(list_next(&freeMSG),msg_t,m_next);
-	list_del(&(extractedMSG->m_next));	
-	return extractedMSG;
+    //extract a message from the first list element
+    struct msg_t* extractedMSG = container_of(list_next(&freeMSG), msg_t, m_next);
+    //remove the extracted message from the free list
+    list_del(&(extractedMSG->m_next));	
+    //return the message
+    return extractedMSG;
 }
