0f3f5e14ab6626d87cf3ec90b23a7376046fc2cb
Riccardo Maffei <RiccardoMaffei@users.noreply.github.com>
2017-03-26 18:51:33 +0200

    Added thread_terminate() to SSI. Improved thread_terminate() implementation. Used thread_terminate() in proc_terminate(). Fixed include hierarchy in ssi.h. Improved documentation in ssi.h.

--- ssi.c
+++ ssi.c
@@ -1,5 +1,4 @@
 #include "ssi.h"
-#include "mikabooq.h"
 
 void SSI_entry_point(){
     //TODO: implement
@@ -26,9 +25,37 @@ void proc_terminate(struct pcb_t* proc){
     //delete threads of the process
     list_for_each_entry(thread, &(proc->p_threads), t_next){
         //terminate thread
-        thread_free(thread);
+        thread_terminate(thread);
     }
     //the process hasn't any threads or children
     //delete the process
     proc_delete(proc);
+}
+
+void thread_terminate(struct tcb_t* thread){
+    //TODO:check which queue contains the thread.
+    //if in wait queue
+        //decrease softblock
+        //softBlockedThread--
+        //update wait structures if necessary
+    //else if in ready queue -> do nothing special
+    
+    //sender of the message
+    struct tcb_t* sender = NULL;
+    //the message payload
+    uintptr_t payload;
+    //while there are pending messages
+    while((msgq_get(&sender, thread, &payload)) != -1){
+        //TODO: discard the message (send discard reply)
+        
+        //reset the sender as NULL
+        sender = NULL;
+    }
+
+    //remove the thread from the queue (no matter which one)
+    list_del(&(thread->t_sched));
+    //decrease the number of threads
+    totalThread--;
+    //free the thread
+    thread_free(thread);
 }
--- ssi.h
+++ ssi.h
@@ -1,13 +1,27 @@
 #ifndef SSI_H
 #define SSI_H
 
+#include "mikabooq.h"
+#include "scheduler.h"
+
 /**
  * System Service Interface entry point.
  */
 void SSI_entry_point();
 
-/* delete a process, its children and threads*/
+/**
+ * Terminates the given process and its whole process tree.
+ * Termination will also delete every process's thread.
+ * @param proc pcb_t*: pointer to the process to delete.
+ */
 void proc_terminate(struct pcb_t* proc);
+
+/**
+ * Terminates the given thread.
+ * Termination will also discard all pending messages.
+ * @param thread tcb_t*: pointer to the thread to delete.
+ */
+void thread_terminate(struct tcb_t* thread);
 
 #endif /* SSI_H */
 
