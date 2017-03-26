1f599a45dfc51d7aaf43a004c85ded284f1eaf12
Teresa Signati <teresa.signati@studio.unibo.it>
2017-03-26 09:20:18 -0700

    Bugfix in proc_terminate().

--- ssi.c
+++ ssi.c
@@ -16,14 +16,16 @@ void proc_terminate(struct pcb_t* proc){
     //declare an iterator
     struct pcb_t* children;
     //terminate the children of the process
-    list_for_each_entry(children, proc->p_children, p_siblings){
+    list_for_each_entry(children, &(proc->p_children), p_siblings){
+        //DFS-style recursive call
         proc_terminate(children);
     }
     //the process has no childern at this point
     //declare an iterator
     struct tcb_t* thread;
     //delete threads of the process
-    list_for_each_entry(thread, proc->p_threads, t_next){
+    list_for_each_entry(thread, &(proc->p_threads), t_next){
+        //terminate thread
         thread_free(thread);
     }
     //the process hasn't any threads or children
