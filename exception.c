4437b637a989c3a622c051d44934bdfd9246aa85
Riccardo Maffei <RiccardoMaffei@users.noreply.github.com>
2017-04-02 11:54:14 +0200

    Added trap_passup() and its simple implementation. Improved tlb_handler() and pgm_trap_handler(). Updated makefile.

--- exception.c
+++ exception.c
@@ -2,13 +2,42 @@
 #include "scheduler.h"
 #include "utils.h"
 #include "uARMconst.h"
+#include "ssi.h"
 
 void tlb_handler(){
     //TODO: implement
+    //TODO: accounting if necessary
+    
+    //copy the old state in the tcb
+    memcopy((state_t*) TLB_OLDAREA, &(runningThread->t_s), sizeof(state_t));
+    //if the process (owner of the running thread) has a TLB trap manager set
+    if(runningThread->t_pcb->tlbMgr != NULL){
+        //pass up the exception
+        trap_passup(runningThread->t_pcb->tlbMgr);
+    }
+    //else (unrecoverable exception)
+    else{
+        //terminate the process, all its threads, and the whole subtree
+        proc_terminate(runningThread->t_pcb);
+    }
 }
 
 void pgm_trap_handler(){
     //TODO: implement
+    //TODO: accounting if necessary
+    
+    //copy the old state in the tcb
+    memcopy((state_t*) PGMTRAP_OLDAREA, &(runningThread->t_s), sizeof(state_t));
+    //if the process (owner of the running thread) has a PGM trap manager set
+    if(runningThread->t_pcb->pgmMgr != NULL){
+        //pass up the exception
+        trap_passup(runningThread->t_pcb->pgmMgr);
+    }
+    //else (unrecoverable exception)
+    else{
+        //terminate the process, all its threads and the whole subtree
+        proc_terminate(runningThread->t_pcb);
+    }
 }
 
 void sys_bk_handler(){
@@ -32,6 +61,36 @@ void sys_bk_handler(){
         //print an error (here interrupts are masked)
         tprint("SYS/BK HANDLER: Something went wrong\n");
         //panic
+        PANIC();
+    }
+}
+
+void trap_passup(struct tcb_t* manager){
+    //send a message to the manager with the cause as payload
+    //and the offending thread as sender. Save the return value.
+    int succeeded = msgq_add(runningThread, manager, runningThread->t_s.CP15_Cause);
+    //if send succeeded
+    if(succeeded == 0){
+        //enqueue in wait queue
+        thread_enqueue(runningThread, &waitQueue);
+        //increase soft blocked threads number
+        softBlockedThread++;
+
+        //TODO: set status as wait for trap
+
+        //remove the thread from running
+        runningThread = NULL;
+        //if the manager is not in ready queue
+        if(manager->t_status != T_STATUS_READY){
+            //TODO: wake manager up
+        }
+        //call the scheduler
+        scheduler();
+    }
+    //else (message limit exceeded)
+    else{
+        //print error message and panic
+        tprint("CAN'T PASS UP TRAP: send failed\n");
         PANIC();
     }
 }
--- exception.h
+++ exception.h
@@ -1,6 +1,8 @@
 #ifndef EXCEPTION_H
 #define EXCEPTION_H
 
+#include "mikabooq.h"
+
 /**
  * TLB exception handler routine.
  */
@@ -15,6 +17,13 @@ void pgm_trap_handler();
  * System call/breakpoint handler routine.
  */
 void sys_bk_handler();
+
+/**
+ * Passes up the exception to the given manager.
+ * This function also moves the running thread to the wait queue and wakes the manager up.
+ * @param manager struct tcb_t*: the trap manager.
+ */
+void trap_passup(struct tcb_t* manager);
 
 #endif /* EXCEPTION_H */
 
